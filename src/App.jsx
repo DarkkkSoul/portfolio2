866540fc22e1f86c03a150da9ca8e13a1fc965f9
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-06-23 22:45:32 +0530

    aboutme page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import './App.css'
-
+import './index.css';
 function App() {
-
     return (
-        <div className=''>
+        <div className='bg-gradient-to-br from-lime-400 from-0% via-green-300 via-60% to-lime-900 h-screen w-full flex items-center justify-center'>
 
+            <div className='bg-white/30 backdrop-blur-lg w-1/2 p-10 rounded-md flex flex-col items-center justify-center gap-y-7'>
+
+                <div className='w-50 rounded-full'>
+                    <img src="/image.jpeg" className='rounded-full' />
+                </div>
+
+                <div className='text-lg'>
+                    <p>
+                        Hey! I'm <span className='text-lime-600 font-extrabold'>Dark Soul</span> a.k.a Mahesh, a 19 year old web developer from India. I love building <span className='font-extrabold'>interactive, unique and scalable</span> interfaces using  <span className='text-lime-600 font-extrabold'>MERN</span> stack.
+                    </p>
+                    <p>
+                        I work solo and fast mostly with Javascript frameworks.
+                    </p>
+                </div>
+
+
+            </div>
         </div>
-    )
+    );
 }
 
-export default App
+export default App  
